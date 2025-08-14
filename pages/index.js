0b85eabf55333ea0110baf89b6f87795c2d674aa
ERIK BARBOSA DA COSTA <79406452+atsireopac@users.noreply.github.com>
2025-08-14 22:36:04 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-    return <h1>Meire, eu amo você. Se você me ama, dá uma risadinha!;)</h1>
+  return <h1>Meire, eu amo você. Se você me ama, dá uma risadinha!;)</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("interação errada");
+}
+
+function teste3() {
+  console.log("errei");
 }
 
 export default Home;
