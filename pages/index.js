3ba3958a058af6f5083c3127b88ac12dcf7cc6fd
ERIK BARBOSA DA COSTA <79406452+atsireopac@users.noreply.github.com>
2025-08-19 14:20:49 +0000

    install `jest` and configure test scripts

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,4 @@ function Home() {
   return <h1>Meire, eu amo você. Se você me ama, dá uma risadinha!;)</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("interação errada");
-}
-
-function teste3() {
-  console.log("errei");
-}
-
 export default Home;
