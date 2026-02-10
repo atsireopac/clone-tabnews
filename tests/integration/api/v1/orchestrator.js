ebf525c3c854ae67ead4c2b0bf8d9f2bdaa5ee0b
ERIK BARBOSA DA COSTA <79406452+atsireopac@users.noreply.github.com>
2026-02-10 14:43:10 +0000

    refactor: use `orchestrator.clearDatabase()` in tests

--- tests/integration/api/v1/orchestrator.js
+++ tests/integration/api/v1/orchestrator.js
@@ -1,4 +1,5 @@
 import retry from "async-retry";
+import database from "infra/database.js";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -6,18 +7,26 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 1000,
     });
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      // eslint-disable-next-line no-unused-vars
-      const responseBody = await response.json();
+
+      if (response.status !== 200) {
+        throw Error();
+      }
     }
   }
 }
 
+async function clearDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
 const orchestrator = {
   waitForAllServices,
+  clearDatabase,
 };
 
 export default orchestrator;
